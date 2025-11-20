66b757993c817c99f3c5c3b0228b81f21bc64d42
VikaKhaustova <105599152+VikaKhaustova@users.noreply.github.com>
2025-11-20 13:33:58 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+// Функція для отримання значення кукі за ім'ям
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';');
+
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
+
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
+        }
+    }
+    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
+    return '';
+}
+
+let themeBtn = document.querySelector("#themeToggle")
+
+function setTheme(theme) {
+    if (theme == 'light') {
+        document.body.classList.add("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
+    } else {
+        document.body.classList.remove("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-brightness-high"></i>';
+    }
+}
+
+let theme = getCookieValue('theme')
+setTheme(theme)
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle('light-theme'); // Перемикаємо клас теми
+    if (theme == 'light') {
+        theme = 'dark'
+    } else {
+        theme = 'light'
+    }
+    setTheme(theme)
+    // Зберігаємо JSON рядок у кукі
+    document.cookie = `theme=${theme}; max-age=${60 * 60 * 24 * 7}; path=/`;
+}) 
+
+// Очікуємо завантаження сторінки
+document.addEventListener('DOMContentLoaded', function() {
+    // Отримуємо всі написи для анімації
+    const textElements = document.querySelectorAll('.fade-in-text');
+
+    // Додаємо клас "show" для запуску анімації
+    textElements.forEach(element => {
+        element.classList.add('show');
+    });
+});
+
+
+//ДОДАТИ ВСЕРЕДИНУ
+
+
+//
+
+
+
+
 let likeButton1 = document.getElementById('like-button1');
 let likeButton2 = document.getElementById('like-button2');
 let likeButton3 = document.getElementById('like-button3');
@@ -82,60 +148,4 @@ likeButton1.addEventListener('click', function() {
             this.classList.add('fas');
         }
     })
-
-    // Функція для отримання значення кукі за ім'ям
-function getCookieValue(cookieName) {
-    // Розділяємо всі куки на окремі частини
-    const cookies = document.cookie.split(';');
-
-    // Шукаємо куки з вказаним ім'ям
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
-
-        // Перевіряємо, чи починається поточне кукі з шуканого імені
-        if (cookie.startsWith(cookieName + '=')) {
-            // Якщо так, повертаємо значення кукі
-            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
-        }
-    }
-    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
-    return '';
-}
-
-let themeBtn = document.querySelector("#themeToggle")
-
-function setTheme (theme) {
-    if (theme='light') {
-        document.body.classList.add("light-theme");
-        themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
-    } else {
-        document.body.classList.remove("light-theme");
-        themeBtn.innerHTML = '<i class="bi bi-brightness-high"></i>';
-    }
-}
-
-let theme = getCookieValue('theme')
-setTheme (theme)
-
-themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle('light-theme'); // Перемикаємо клас теми
-    if (theme == 'light') {
-        theme ='dark'
-    } else {
-        theme ='light'
-    }
-    setTheme (theme)
-    // зберігаємо JSON рядок у кукі
-    document.cookie = `theme=${theme}; max-age=${60 * 60 * 24 * 7}; path=/`;
-})
-
-// Очікуємо завантаження сторінки
-document.addEventListener('DOMContentLoaded', function() {
-    // Отримуємо всі написи для анімації
-    const textElements = document.querySelectorAll('.fade-in-text');
-
-    // Додаємо клас "show" для запуску анімації
-    textElements.forEach(element => {
-        element.classList.add('show');
-    });
-});
+  
